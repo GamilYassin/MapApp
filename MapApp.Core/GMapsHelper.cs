2508e1526c934c443e38e8b180f8f7ad82292390
Gamgom Go <gamil.yassin@gmail.com>
2019-11-07 02:47:42 +0300

    cleaned code

--- MapApp.Core/GMapsHelper.cs
+++ MapApp.Core/GMapsHelper.cs
@@ -1,11 +1,8 @@
-﻿using System;
+﻿using GMap.NET.MapProviders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using GMap.NET;
-using GMap.NET.MapProviders;
 
 namespace MapApp.Core
 {
--- MapApp.GUI/Form1.cs
+++ MapApp.GUI/Form1.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using MapApp.Core;
-using GMap.NET;
+﻿using GMap.NET;
 using GMap.NET.MapProviders;
 using GMap.NET.WindowsForms;
 using GMap.NET.WindowsForms.Markers;
+using MapApp.Core;
+using System;
+using System.Windows.Forms;
 
 namespace MapApp.GUI
 {
